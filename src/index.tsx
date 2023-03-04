c1e1778ab87d6b17b5f082575826c3f618079de2
yaroslav skiba <109871666+yaroslavskiba@users.noreply.github.com>
2023-03-04 14:50:32 +0300

    structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import store from './slices/index';
+// import { Provider, useDispatch, useSelector } from 'react-redux';
 
 const App = () => {
   return (
     <>
-      <p>Hello 25/5O</p>
+      <h1>25 + 5 Clock</h1>
+
+      <div className='break-session'>
+
+        <div className='break-container'>
+          <span id="break-label">Break Length</span>
+          <div className='buttons'>
+            <button></button>
+            <span>{ }</span>
+            <button></button>
+          </div>
+        </div>
+
+        <div className='session-container'>
+          <span id="session-label">Session Length</span>
+          <div className='buttons'>
+            <button></button>
+            <span>{ }</span>
+            <button></button>
+          </div>
+        </div>
+      </div>
+
+      <div className='session'>
+        <h2>session</h2>
+        <span className='timer'>{ }</span>
+      </div>
+
+      <div className='control-buttons'>
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
     </>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  // <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  {/* </Provider>, */}
 );
